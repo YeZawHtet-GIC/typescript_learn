04c1130d889d51dd79cbb72a2dc4d231a947b2f5
YeZawHtet-GIC <yezawhtet@gicjp.com>
2024-03-04 15:00:27 +0630

    added auto compile ts file and auto view js file

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,13 @@
 let age = 30;
 console.log(age);
 //!compiling typescript in new js file
-//tsc index.ts
+//?tsc index.ts
 //!running compiled js file
-//node index.js
+//?node index.js
+//!auto generate console 
+//? node --watch index.js
 let moon = 1;
 let sun = 1;
 let isNight = false;
 console.log("this is test for tsc files");
+console.log("hello world!");
