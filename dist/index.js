51eced479486ac4a6f61d8acb885e2808f8daad3
YeZawHtet-GIC <yezawhtet@gicjp.com>
2024-03-04 14:40:21 +0630

    auto compiling the ts file data into js file

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,11 @@
+"use strict";
 let age = 30;
 console.log(age);
-
 //!compiling typescript in new js file
 //tsc index.ts
 //!running compiled js file
 //node index.js
-
 let moon = 1;
 let sun = 1;
 let isNight = false;
-
-moon = "2";
-sun = "3";
-isNight = "false";
+console.log("this is test for tsc files");
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let age = 30;
+console.log(age);
+
+//!compiling typescript in new js file
+//tsc index.ts
+//!running compiled js file
+//node index.js
+
+let moon = 1;
+let sun = 1;
+let isNight = false;
+console.log("this is test for tsc files");
